b7a9e5b147d03de3c034a2b2c50cd7c7e70749ed
Simp4Codes <70384785+Simp4Codes@users.noreply.github.com>
2021-01-25 18:33:26 +0400

    Add files via upload

---
+++ BootPanel/BootPanel/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BootPanel
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "simp4codes" && textBox2.Text == "1234")
+            {
+                Form gg = new Form();
+                gg.Show();
+                this.Hide();
+            }
+                
+        }
+    }
+}
